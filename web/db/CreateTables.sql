14fae8212289bf7d5f16de3e8dd7ab8c9744cf3d
Dennis Z <3003dennis@gmail.com>
2020-05-13 15:39:44 +1000

    Added account table to create tables script

---
+++ web/db/CreateTables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Account (
+    "userID" VARCHAR(50),
+    "userEmail" VARCHAR(50),
+    "userFName" VARCHAR(20),
+    "userLName" VARCHAR(10),
+    "accountType" VARCHAR(1),
+    PRIMARY KEY (userID)
+);
