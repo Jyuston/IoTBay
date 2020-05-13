99b1dc3995dae7c3f6523c7a0983a9e9c338f3e6
Dennis Z <3003dennis@gmail.com>
2020-05-13 15:46:48 +1000

    increased length of fields for account table

--- web/db/CreateTables.sql
+++ web/db/CreateTables.sql
@@ -2,7 +2,7 @@ CREATE TABLE Account (
     "userID" VARCHAR(50),
     "userEmail" VARCHAR(50),
     "userFName" VARCHAR(20),
-    "userLName" VARCHAR(10),
+    "userLName" VARCHAR(20),
     "accountType" VARCHAR(1),
     PRIMARY KEY (userID)
 );
