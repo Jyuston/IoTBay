ae10611004700c6dec74109c3c341b4a301caaae
Dennis Z <3003dennis@gmail.com>
2020-05-19 01:11:20 +1000

    inital commit

--- src/java/uts/isd/model/reporting/TotalSales.java
+++ src/java/uts/isd/model/reporting/TotalSales.java
@@ -9,6 +9,7 @@ public class TotalSales extends ReportElement implements Serializable {
     public TotalSales(String elementName, String elementDescription) {
         super(elementName, elementDescription);
         // TODO Auto-generated constructor stub
+        // INIT Commit
     }
 
     public void use(ArrayList<TotalSalesRecord> records) {
