2966cbfe667f30cffc1f6c72cda51fb2b6f04428
Dennis Z <3003dennis@gmail.com>
2020-05-18 17:55:19 +1000

    added started files

---
+++ src/java/uts/isd/model/reporting/TotalSales.java
@@ -0,0 +1,12 @@
+package uts.isd.model.reporting;
+
+import java.io.Serializable;
+
+public class TotalSales extends ReportElement implements Serializable {
+
+    public TotalSales(String elementName, String elementDescription) {
+        super(elementName, elementDescription);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
