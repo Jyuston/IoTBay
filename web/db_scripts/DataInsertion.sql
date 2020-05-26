fde4acc5fa0e01cad1faecd9a17149d2967862bc
vkumar12180 <62045253+vkumar12180@users.noreply.github.com>
2020-05-26 22:14:51 +1000

    added 10 products

--- web/db_scripts/DataInsertion.sql
+++ web/db_scripts/DataInsertion.sql
@@ -69,23 +69,28 @@ VALUES
     ('P-0000004', '0.5 force sensitive resistor sensor', 25, 8.50, 'Resistor', 'This is a force sensitive resistor with a round, 0.5" diameter, sensing area.', false),
     ('P-0000005', '2.5 micro farahs capacitor', 20, 1.50, 'Capacitors', 'This is a micro farahs capacitor', false),
     ('P-0000006', '1/4W resistor kit', 30, 12.00, 'Resistors', 'Resistor kit comes with 25 each of 20 different resistor values ', false),
-    ('P-0000007', 'cermaic capacitor', 22, 3.50, 'Capacitors', 'This is a micro farahs capacitor', false),
-    ('P-0000008', 'tantalum capacitor', 20, 4.00 'Capacitors', 'This is a micro farahs capacitor', false),
-    ('P-0000010', 'aluminium capacitor', 20, 1.50, 'Capacitors', 'This is a micro farahs capacitor', false),
-    ('P-0000010', 'aluminium capacitor', 20, 1.50, 'Capacitors', 'This is a micro farahs capacitor', false),
-    ('P-0000011', '2.5 micro farahs capacitor', 20, 1.50, 'Capacitors', 'This is a micro farahs capacitor', false),
-    ('P-0000012', '2.5 micro farahs capacitor', 20, 1.50, 'Capacitors', 'This is a micro farahs capacitor', false),
-    ('P-0000013', '2.5 micro farahs capacitor', 20, 1.50, 'Capacitors', 'This is a micro farahs capacitor', false),
-    ('P-0000014', '2.5 micro farahs capacitor', 20, 1.50, 'Capacitors', 'This is a micro farahs capacitor', false),
-    ('P-0000015', '2.5 micro farahs capacitor', 20, 1.50, 'Capacitors', 'This is a micro farahs capacitor', false),
-    ('P-0000016', '2.5 micro farahs capacitor', 20, 1.50, 'Capacitors', 'This is a micro farahs capacitor', false),
-    ('P-0000017', '2.5 micro farahs capacitor', 20, 1.50, 'Capacitors', 'This is a micro farahs capacitor', false),
+    ('P-0000007', 'ceramic capacitor', 22, 3.50, 'Capacitors', 'This is a ceramic capacitor', false),
+    ('P-0000008', 'tantalum capacitor', 20, 4.00 'Capacitors', 'This is a tantalum capacitor', false),
+    ('P-0000010', 'plastic film capacitor', 20, 1.50, 'Capacitors', 'This is a plastic film capacitor', false),
+    ('P-0000010', 'aluminium capacitor', 30, 5.50, 'Capacitors', 'This is a aluminium capacitor', false),
+    ('P-0000011', 'electrolytic capacitor', 40, 6.00, 'Capacitors', 'This is a electrolytic capacitor', false),
+    ('P-0000012', 'carbon film resistor', 20, 0.50, 'Resistors', 'This is a carbon film resistor', false),
+    ('P-0000013', 'metal film resistor', 10, 1.50, 'Resistors', 'This is a metal film resistor', false),
+    ('P-0000014', 'wirewound resistor', 20, 4.50, 'Resistors', 'This is a wirewound resistor', false),
+    ('P-0000015', 'power resistor', 50, 9.50, 'Resistors', 'This is a power resistor', false),
+    ('P-0000016', 'load resistor', 90, 10.00, 'Resistors', 'This is a load resistor', false),
+    ('P-0000017', 'voltage led load resistor', 20, 1.50, 'Resistors', 'This is a voltage led load resistor', false),
+    ('P-0000018', 'high temp electrolytic capacitors', 80, 7.00, 'Capacitors', 'This is a high temp capacitor', false),
+    ('P-0000019', 'microfarad run capacitor', 20, 1.50, 'Capacitors', 'This is a microfarad run capacitor', false),
+    ('P-0000020', 'plastic capacitor', 10, 0.50, 'Capacitors', 'This is a plastic capacitor', false)
+    
 ;
 
 INSERT INTO ORDERS (ORDER_ID, ORDER_DATE_TIME, ORDER_SHIPPING_ADDRESS, ORDER_TOTAL, USER_ID, TRACKING_ID, CARD_NUMBER)
 VALUES
     ('O-0000001', '2020-01-01 00:00:05', '21 Shipping Ave--Waterloo-2017-NSW', 23.99, 'U-00000002', '00000000000', '5244 8746 5799 2270'),
-    ('O-0000002', '2020-01-02 00:00:05', '30 Shipping Ave--Waterloo-3105-QLD', 28.00, 'U-00000002', '00000000000', '5244 8746 5799 2271')
+    ('O-0000002', '2020-01-02 00:00:05', '30 Shipping Ave--Waterloo-3105-QLD', 28.00, 'U-00000002', '00000000000', '5244 8746 5799 2271'),
+    
 ;
 
 INSERT INTO ORDER_LINE (ORDER_ID, PRODUCT_ID, QUANTITY_ORDERED, PRODUCT_PRICE)
