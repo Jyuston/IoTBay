03cfbd57830ce637b2bb86d7dfbb5800f4b3acd2
LonJonn <leon.salsiccia@gmail.com>
2020-05-27 16:38:13 +1000

    Nicer way to access db connection

--- src/java/uts/isd/controller/ConnServlet.java
+++ src/java/uts/isd/controller/ConnServlet.java
@@ -3,28 +3,29 @@ package uts.isd.controller;
 import uts.isd.model.dao.DBConnector;
 
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ConnServlet extends HttpServlet {
-    private final DBConnector connector = new DBConnector();
-
+    /**
+     * Open a singleton db connection on application deploy.
+     * This connection can be used in DAOs by calling `DBConnector.getConnection()`
+     */
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    public void init() {
         try {
-            Connection dbConnection = connector.getConnection();
-            request.getSession().setAttribute("dbConnection", dbConnection);
+            DBConnector.openConnection();
         } catch (ClassNotFoundException | SQLException err) {
             err.printStackTrace();
         }
     }
 
+    /**
+     * Close the singleton db connection when application is stopped.
+     */
     @Override
     public void destroy() {
         try {
-            connector.closeConnection();
+            DBConnector.closeConnection();
         } catch (SQLException err) {
             err.printStackTrace();
         }
--- src/java/uts/isd/model/dao/DBConnector.java
+++ src/java/uts/isd/model/dao/DBConnector.java
@@ -3,27 +3,28 @@ package uts.isd.model.dao;
 import java.sql.*;
 
 public class DBConnector {
-    private final String URL = "jdbc:derby://localhost:1527/testdb";
-    private final String USER = "TST";
-    private final String PASS = "admin";
-    private final String driver = "org.apache.derby.jdbc.ClientDriver";
+    private static final String URL = "jdbc:derby://localhost:1527/testdb";
+    private static final String USER = "TST";
+    private static final String PASS = "admin";
+    private static final String driver = "org.apache.derby.jdbc.ClientDriver";
+    private static Connection dbConnection = null;
 
-    private Connection dbConnection;
-
-    /**
-     * Used in the ConnServlet to establish a single connection
-     * to be used throughout the session.
-     *
-     * @return Connection to the database
-     */
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
+    public static void openConnection() throws ClassNotFoundException, SQLException {
         Class.forName(driver);
         dbConnection = DriverManager.getConnection(URL, USER, PASS);
+    }
 
+    /**
+     * Returns the singleton database connection to be used throughout the app.
+     * Will create an initial connection if no connection had been made previously.
+     *
+     * @return Connection object
+     */
+    public static Connection getConnection() {
         return dbConnection;
     }
 
-    public void closeConnection() throws SQLException {
+    public static void closeConnection() throws SQLException {
         dbConnection.close();
     }
 }
