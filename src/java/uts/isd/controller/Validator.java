e95fe83e49aa5100745a6c7f263a1d473e982e25
Dennis Z <3003dennis@gmail.com>
2020-06-06 17:37:56 +1000

    added system admin control to feature

--- src/java/uts/isd/controller/Validator.java
+++ src/java/uts/isd/controller/Validator.java
@@ -65,7 +65,7 @@ public class Validator {
     
         if (startDate.compareTo(endDate) > 0) {
             failed = true;
-            request.setAttribute("error", "The report end date cannot be before the report start date");
+            request.setAttribute("error", "The report end date cannot be before the report start date.");
         }
         
         return this;
