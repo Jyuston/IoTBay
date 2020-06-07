3116cdf01a0702965d6a19c2db02f02d07991b6a
Dennis Z <3003dennis@gmail.com>
2020-06-08 03:15:22 +1000

    DAO refacotring to use prepared statements

--- src/java/uts/isd/model/dao/ReportingDAO.java
+++ src/java/uts/isd/model/dao/ReportingDAO.java
@@ -5,24 +5,22 @@ import uts.isd.model.reporting.Report;
 import uts.isd.model.reporting.OrderLineItem;
 
 import java.sql.*;
-import java.text.ParseException;
 import java.util.*;
 
 public class ReportingDAO {
     public static final Connection dbConnection = DBConnector.getConnection();
 
-    public static ArrayList<OrderLineItem> totalSales(String beginTimeStamp, String endTimeStamp)
-            throws SQLException, DAOException {
-        // Set up the inital SQL query hre
-        Statement st = dbConnection.createStatement();
+    public static ArrayList<OrderLineItem> totalSales(String beginTimeStamp, String endTimeStamp) throws SQLException, DAOException {
+        // Set up the inital SQL query here
+        String query = "SELECT ORDER_LINE.PRODUCT_ID, NAME, CATEGORY, QUANTITY_ORDERED, ORDER_LINE.PRICE, SHIPPING_ADDRESS " +
+                "FROM ORDERS " + "INNER JOIN ORDER_LINE on ORDERS.ID = ORDER_LINE.ORDER_ID " +
+                "INNER JOIN PRODUCTS on ORDER_LINE.PRODUCT_ID = PRODUCTS.ID " + "WHERE ORDERS.ORDERED_ON > ? "+
+                "AND ORDERS.ORDERED_ON < ?";
 
-        String query = "SELECT ORDER_LINE.PRODUCT_ID, NAME, CATEGORY, QUANTITY_ORDERED, ORDER_LINE.PRICE, SHIPPING_ADDRESS "
-                + "FROM ORDERS " + "INNER JOIN ORDER_LINE on ORDERS.ID = ORDER_LINE.ORDER_ID "
-                + "INNER JOIN PRODUCTS on ORDER_LINE.PRODUCT_ID = PRODUCTS.ID " + "WHERE ORDERS.ORDERED_ON > '"
-                + beginTimeStamp + "' AND ORDERS.ORDERED_ON < '" + endTimeStamp + "'";
+        PreparedStatement st = DAOUtils.prepareStatement(query, false, beginTimeStamp, endTimeStamp);
 
         // Execute the query and store the results in the result set
-        ResultSet queryResult = st.executeQuery(query);
+        ResultSet queryResult = st.executeQuery();
 
         ArrayList<OrderLineItem> queryResultsStructured = new ArrayList<OrderLineItem>();
 
@@ -38,15 +36,16 @@ public class ReportingDAO {
     }
 
     public static ArrayList<String> categories(String beginTimeStamp, String endTimeStamp) throws SQLException {
-        // Set up the inital SQL query here
-        Statement st = dbConnection.createStatement();
-        String query = "SELECT distinct(CATEGORY) FROM PRODUCTS P, ORDERS O "
-                + "WHERE (P.ID in (SELECT PRODUCT_ID FROM ORDER_LINE)) " 
-                + "AND (O.ORDERED_ON > '" + beginTimeStamp
-                + "' AND O.ORDERED_ON < '" + endTimeStamp + "')";
+        String query = 
+            "SELECT distinct(CATEGORY) FROM PRODUCTS P, ORDERS O " + 
+            "WHERE (P.ID in (SELECT PRODUCT_ID FROM ORDER_LINE)) " + 
+            "AND (O.ORDERED_ON > ? " +
+            "AND O.ORDERED_ON < ?)";
+
+        PreparedStatement st = DAOUtils.prepareStatement(query, false, beginTimeStamp, endTimeStamp);
 
         // Execute the query and store the results in the result set
-        ResultSet queryResult = st.executeQuery(query);
+        ResultSet queryResult = st.executeQuery();
 
         ArrayList<String> categories = new ArrayList<>();
 
@@ -57,18 +56,18 @@ public class ReportingDAO {
         return categories;
     }
 
-    public static ArrayList<ProductSummary> productSnapshots(String beginTimeStamp, String endTimeStamp) throws SQLException {
-        Statement st = dbConnection.createStatement();
-        
+    public static ArrayList<ProductSummary> productSnapshots(String beginTimeStamp, String endTimeStamp) throws SQLException {        
         String query = "SELECT p.id, p.\"NAME\", p.CATEGORY, SUM(ol.QUANTITY_ORDERED) AS TotalQuantity, SUM(ol.QUANTITY_ORDERED * ol.PRICE) AS TotalPrice " +
                 "FROM ORDER_LINE ol " +
                 "INNER JOIN PRODUCTS p ON ol.PRODUCT_ID = p.ID " +
                 "INNER JOIN ORDERS o ON ol.ORDER_ID = o.ID " + 
-                "WHERE o.ORDERED_ON > '" + beginTimeStamp + "' AND o.ORDERED_ON < '" + endTimeStamp + "' " +
+                "WHERE o.ORDERED_ON > ? AND o.ORDERED_ON < ? " +
                 "GROUP BY p.ID, p.\"NAME\", p.CATEGORY";
+       
+        PreparedStatement st = DAOUtils.prepareStatement(query, false, beginTimeStamp, endTimeStamp);
 
         // Execute the query and store the results in the result set
-        ResultSet queryResult = st.executeQuery(query);
+        ResultSet queryResult = st.executeQuery();
 
         ArrayList<ProductSummary> snapshots = new ArrayList<>();
 
@@ -83,11 +82,11 @@ public class ReportingDAO {
     }
 
     public static ArrayList<String> getReportNames() throws SQLException {
-        Statement st = dbConnection.createStatement();
-
         String query = "SELECT NAME FROM REPORTS";
 
-        ResultSet queryResult = st.executeQuery(query);
+        PreparedStatement st = DAOUtils.prepareStatement(query, false);
+
+        ResultSet queryResult = st.executeQuery();
 
         ArrayList<String> reportNames = new ArrayList<>();
 
@@ -99,14 +98,13 @@ public class ReportingDAO {
     }
 
     public static ArrayList<ProductSummary> getProductStock() throws SQLException {
-        Statement st = dbConnection.createStatement();
-
         // Retrievs a product and its stock if its not archived
         String query = "SELECT ID, NAME, CATEGORY, STOCK " +
             "FROM PRODUCTS " +
             "WHERE CATEGORY IN (SELECT CATEGORY FROM PRODUCT_CATEGORIES WHERE NOT ARCHIVED)" ;
 
-        ResultSet queryResult = st.executeQuery(query);
+        PreparedStatement st = DAOUtils.prepareStatement(query, false);
+        ResultSet queryResult = st.executeQuery();
 
         ArrayList<ProductSummary> snapshots = new ArrayList<>();
 
@@ -120,13 +118,12 @@ public class ReportingDAO {
     }
 
     public static ArrayList<String> getProductCategories() throws SQLException {
-        Statement st = dbConnection.createStatement();
-
         // Gets a category if it has not been archived
         String query = "SELECT CATEGORY " + 
             "FROM PRODUCTS";
 
-        ResultSet queryResult = st.executeQuery(query);
+        PreparedStatement st = DAOUtils.prepareStatement(query, false);
+        ResultSet queryResult = st.executeQuery();
 
         ArrayList<String> categories = new ArrayList<>();
 
@@ -138,31 +135,28 @@ public class ReportingDAO {
     }
 
     // Create
-    public static Report save(String[] params) throws SQLException, ParseException {
+    public static Report save(String[] params) throws SQLException {
         checkDuplicate(params[0]);
 
-        Statement st = dbConnection.createStatement();
-
         String query = 
-            "INSERT INTO REPORTS (NAME, DESCRIPTION, START_DATE, END_DATE) " +
-            "VALUES " +
-            "('" + params[0] + "', '" + params[1] + "', '" + 
-            params[2] + " 00:00:00', '" + params[3] + " 23:59:59')";
+        "INSERT INTO REPORTS (NAME, DESCRIPTION, START_DATE, END_DATE) " +
+        "VALUES (?, ?, ?, ?)";
 
-        int rowsChanged = st.executeUpdate(query);
+        PreparedStatement st = DAOUtils.prepareStatement(query, false, params[0], params[1], params[2] + " 00:00:00", params[3] + " 23:59:59");
+        int rowsChanged = st.executeUpdate();
 
-        checkExecutionFails(rowsChanged);
+        checkExecutionFailed(rowsChanged);
 
         return get(params[0]);
     }
 
     // Read
     public static Report get(String name) throws SQLException {
-        Statement st = dbConnection.createStatement();
         
-        String query = "SELECT NAME, DESCRIPTION, START_DATE, END_DATE FROM REPORTS WHERE NAME = '" + name + "'";
+        String query = "SELECT NAME, DESCRIPTION, START_DATE, END_DATE FROM REPORTS WHERE NAME = ?";
 
-        ResultSet queryResult = st.executeQuery(query);
+        PreparedStatement st = DAOUtils.prepareStatement(query, false, name);
+        ResultSet queryResult = st.executeQuery();
 
         queryResult.next();
 
@@ -175,13 +169,11 @@ public class ReportingDAO {
     }
 
     // Read
-    public static ArrayList<Report> getAll() throws SQLException {
-        Statement st = dbConnection.createStatement();
-        
+    public static ArrayList<Report> getAll() throws SQLException {        
         String query = "SELECT * FROM reports";
         
-        // Execute the query and store the results in a result set
-        ResultSet queryResult = st.executeQuery(query);
+        PreparedStatement st = DAOUtils.prepareStatement(query, false);
+        ResultSet queryResult = st.executeQuery();
 
         ArrayList<Report> reports = new ArrayList<>();
 
@@ -198,35 +190,32 @@ public class ReportingDAO {
     }
 
     // Update
-    public static Report update(String reportName, String[] params, Boolean nameChanged) throws SQLException, DAOException, ParseException {
+    public static Report update(String reportName, String[] params, Boolean nameChanged) throws SQLException, DAOException {
         // If the name of the report was changed, a check must be done to ensure the name is unique
         if (nameChanged) {
             checkDuplicate(params[0]);
         }
 
-        Statement st = dbConnection.createStatement();
+        String query =
+            "UPDATE REPORTS SET NAME = ?, DESCRIPTION = ?, START_DATE = ?, END_DATE = ? " +
+            "WHERE NAME = ?";
 
-        String query = 
-            "UPDATE REPORTS set NAME = '" + params[0] + "', DESCRIPTION = '" + params[1] + 
-            "', START_DATE = '" + params[2] + " 00:00:00', END_DATE = '" + params[3] + " 23:59:59' " +
-            "WHERE NAME = '" + reportName + "'";
+        PreparedStatement st = DAOUtils.prepareStatement(query, false, params[0], params[1], params[2] + " 00:00:00", params[3] + " 23:59:59", reportName);
+        int rowsChanged = st.executeUpdate();
 
-        int rowsChanged = st.executeUpdate(query);
-
-        checkExecutionFails(rowsChanged);
+        checkExecutionFailed(rowsChanged);
 
         return get(params[0]);
     }
 
     // Delete
     public static void delete(Report r) throws SQLException, DAOException {
-        Statement st = dbConnection.createStatement();
+        String query = "DELETE FROM REPORTS WHERE NAME = ?";
+        
+        PreparedStatement st = DAOUtils.prepareStatement(query, false, r.getName());
+        int rowsChanged = st.executeUpdate();
 
-        String query = "DELETE FROM REPORTS WHERE NAME = '" + r.getName() + "'";
-
-        int rowsChanged = st.executeUpdate(query);
-
-        checkExecutionFails(rowsChanged);        
+        checkExecutionFailed(rowsChanged);        
     }
     
     // Helper Functions
@@ -241,7 +230,7 @@ public class ReportingDAO {
         }
     }
 
-    private static void checkExecutionFails(int i) {
+    private static void checkExecutionFailed(int i) {
         if (i == 0) {
             throw new DAOException("Execution failed in DAO. No rows were changed.");
         }
