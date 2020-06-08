6967cd96445eb87cfac44c71a7901f4c0c502717
OliverS <oliver.sarker1911@gmail.com>
2020-06-08 15:27:48 +1000

    Add search functionality

--- src/java/uts/isd/controller/LogsServlet.java
+++ src/java/uts/isd/controller/LogsServlet.java
@@ -2,7 +2,13 @@ package uts.isd.controller;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import uts.isd.model.Log;
 import uts.isd.model.Account;
@@ -39,6 +45,30 @@ public class LogsServlet extends HttpServlet {
             request.getRequestDispatcher("/logs.jsp").include(request, response);
         }
     }
-    }
     
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        
+        String date = request.getParameter("date");
+        
+        try {
+        HttpSession session = request.getSession();
+        Account account = (Account) session.getAttribute("user");
+        List<Log> dateList = UserAccessDAO.getDate(account.getID(), date);
+        request.setAttribute("userLogs", dateList);
+        }
+
+        catch (SQLException err) {
+            request.setAttribute("logErr", "Error accessing database.");
+            err.printStackTrace();
+        }
+
+        finally {
+            request.getRequestDispatcher("/logs.jsp").include(request, response);
+        }
+        
+    }
+}
+
+   
 
